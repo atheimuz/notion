11ab385c72131dc962a8ccfd411966e444f3948c
atheimuz <atheimuz@naver.com>
2024-08-15 16:15:25 +0900

    스타일 변경

--- src/components/block/Bookmark/Bookmark.js
+++ src/components/block/Bookmark/Bookmark.js
@@ -9,17 +9,17 @@ const Bookmark = ({ data, view }) => {
 
     return (
         <a className={cx(style.bookmark)} href={data.properties?.link?.[0]}>
-            <div className={style.coverWrapper}>
-                <img
-                    src={data.format?.bookmark_cover}
-                    className={style.cover}
-                />
-            </div>
             <div className={style.textBox}>
                 <p className={style.title}>{data.properties?.title?.[0]}</p>
                 <p className={style.desc}>
                     {data.properties?.description?.[0]}
                 </p>
+            </div>
+            <div className={style.coverWrapper}>
+                <img
+                    src={data.format?.bookmark_cover}
+                    className={style.cover}
+                />
             </div>
         </a>
     );
